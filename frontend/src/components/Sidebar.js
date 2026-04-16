fcababe6e7a3581defe3d1bdbe6ee7eaf96dad32
teja <2303A52298@sru.edu.in>
2026-04-16 21:37:38 +0530

    Fixed sidebar error

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,28 +1,36 @@
+import React from "react";
+
 function Sidebar({ setView, active }) {
+
+  const changeView = (view) => {
+    setView(view);
+  };
+
   return (
     <div className="sidebar">
       <h2>💰 FinTrack</h2>
 
       <div
         className={`nav-item ${active === "dashboard" ? "active" : ""}`}
-        onClick={() => setView("dashboard")}
+        onClick={() => changeView("dashboard")}
       >
         Dashboard
       </div>
 
       <div
         className={`nav-item ${active === "analytics" ? "active" : ""}`}
-        onClick={() => setView("analytics")}
+        onClick={() => changeView("analytics")}
       >
         Analytics
       </div>
 
       <div
         className={`nav-item ${active === "expenses" ? "active" : ""}`}
-        onClick={() => setView("expenses")}
+        onClick={() => changeView("expenses")}
       >
         Expenses
       </div>
+
     </div>
   );
 }
